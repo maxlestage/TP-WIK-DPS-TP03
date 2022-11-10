925b218b6b4e2a320bcf9849495189a2c1348e34
maxlestage <maxlestage@icloud.com>
2022-11-10 12:22:43 +0100

    TP3 Done✅

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+use actix_web::{get, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use std::collections::HashMap;
+use std::env;
+use std::io;
+
+#[get("/ping")]
+async fn ping(req: HttpRequest) -> impl Responder {
+    let mut map = HashMap::new();
+    printhostname().unwrap();
+    for header in req.headers().iter() {
+        map.insert(header.0.to_string(), header.1.to_str().unwrap());
+    }
+
+    let serialized_data = serde_json::to_string(&map).unwrap();
+
+    HttpResponse::Ok()
+        .append_header(("content-type", "application/json"))
+        .body(serialized_data)
+}
+
+async fn other(_path: web::Path<String>) -> impl Responder {
+    HttpResponse::NotFound()
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let key: &str = "PING_LISTEN_PORT";
+    let port = match env::var(key) {
+        Ok(val) => val,
+        Err(_) => 8080.to_string(),
+    };
+
+    HttpServer::new(|| {
+        App::new()
+            .service(ping)
+            .route("/{name}", web::get().to(other))
+    })
+    .bind(("0.0.0.0", port.parse().unwrap()))?
+    .run()
+    .await
+}
+
+fn printhostname() -> io::Result<()> {
+    let name = hostname::get()?;
+    println!("{}", name.to_string_lossy());
+    Ok(())
+}
